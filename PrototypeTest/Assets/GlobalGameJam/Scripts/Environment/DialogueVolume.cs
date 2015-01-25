dfcdca570fca21fbb7da5af00d86e9c60bd17ec5
Tyler Kirk <TylerKinkade@gmail.com>
2015-01-24 23:32:06 -0800

    Added new text dialog block.

---
+++ PrototypeTest/Assets/GlobalGameJam/Scripts/Environment/DialogueVolume.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Seasons
+{
+	public class DialogueVolume : LevelObject 
+	{
+		[SerializeField] private string _text; 
+		protected override void EnteredObject (GameObject targetObject)
+		{
+			base.EnteredObject (targetObject);
+			GameObject obj = Instantiate(Resources.Load<GameObject>("UI/WorldSpaceText"),collider.bounds.center,transform.rotation) as GameObject;
+			obj.GetComponent<TextUIController>().RenderText(_text);
+		}
+	}
+}
