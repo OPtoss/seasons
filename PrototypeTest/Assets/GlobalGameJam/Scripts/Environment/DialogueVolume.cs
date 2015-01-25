135a7f768bfd6021382552a8ff539fb95819c5d0
Tyler Kirk <TylerKinkade@gmail.com>
2015-01-24 23:36:00 -0800

    Addded distance field stuff.

--- PrototypeTest/Assets/GlobalGameJam/Scripts/Environment/DialogueVolume.cs
+++ PrototypeTest/Assets/GlobalGameJam/Scripts/Environment/DialogueVolume.cs
@@ -6,10 +6,13 @@ namespace Seasons
 	{
 		[SerializeField] private string _text; 
 		[SerializeField] private Color _textColor;
+		[SerializeField] private float _textDistance = 15f;
 		protected override void EnteredObject (GameObject targetObject)
 		{
 			base.EnteredObject (targetObject);
-			GameObject obj = Instantiate(Resources.Load<GameObject>("UI/WorldSpaceText"),collider.bounds.center,transform.rotation) as GameObject;
+			GameObject obj = Instantiate(Resources.Load<GameObject>("UI/WorldSpaceText"),
+			                             collider.bounds.center + (Vector3.right * _textDistance),
+			                             transform.rotation) as GameObject;
 			obj.GetComponent<TextUIController>().RenderText(_text, _textColor);
 		}
 	}
